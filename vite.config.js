b60b477d827863aa49f23558cdf2951132a9cd5e
activecatalyst <pontiff-snit6p@icloud.com>
2026-04-23 00:02:39 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'inflow' with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/inflow/',
+  base: '/InFlow/',
 })
