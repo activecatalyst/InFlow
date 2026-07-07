84f07dc8f6a86bf43d891b36491e36d91bafaaf1
Eric Fox <Eric@Mac.attlocal.net>
2026-07-06 20:58:39 -0700

    switch to sync code

--- src/firebase.js
+++ src/firebase.js
@@ -3,19 +3,18 @@
 // credential. Access is actually controlled by the Firestore security rules
 // (see FIREBASE_SETUP.md), so it's fine that this file ships to the browser.
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBF1wy780toM--A7lno8ioN4w5gqDQzfNM",
-  authDomain: "inflow-tracker.firebaseapp.com",
-  projectId: "inflow-tracker",
-  storageBucket: "inflow-tracker.firebasestorage.app",
-  messagingSenderId: "730978035364",
-  appId: "1:730978035364:web:d86fa6b8e0cd6c3a4ee831",
+  apiKey: "AIzaSyA8Hd6F9oinSOVmwmj3AYwfDfSd4ilm664",
+  authDomain: "inflow-tracker-d4cc0.firebaseapp.com",
+  projectId: "inflow-tracker-d4cc0",
+  storageBucket: "inflow-tracker-d4cc0.firebasestorage.app",
+  messagingSenderId: "449019882248",
+  appId: "1:449019882248:web:cbe523e01b624e36b09abd",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider();
